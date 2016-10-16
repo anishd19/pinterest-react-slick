f21b2dad3c03e8a7e177f11af7c95d1d08fcfecc
Anish Dhamodaran <anishd19@gmail.com>
2016-10-17 03:52:52 +0530

    last block

--- client/src/js/main.jsx
+++ client/src/js/main.jsx
@@ -10,18 +10,50 @@ var App = React.createClass({
       pinImages: []
     });
   },
+
   componentDidMount: function() {
     this.fetchPins();
   },
+
   fetchPins: function() {
+
+    var self = this;
+
     fetch("https://api.pinterest.com/v1/boards/jyothigandhi/vada//pins/?access_token=AViJPs99Ckr9Bgo88xNa45KVeFvZFH4Zp-70AEVDe3GQ0eBCewAAAAA&fields=id%2Cimage", {
       method: 'get'
     }).then(function(response){
-      console.log(response);
+      response.json().then(function(json) {
+        console.log(json.data);
+        let b = json.data.map((a) => {
+          return(a.image)
+        });
+        let c = b.map((c) => {
+          return(c.original)
+        })
+        let urls = c.map((d) => {
+          return d.url
+        })
+        console.log(urls);
+        self.constructDivs(urls);
+      })
     }).catch(function(err){
       console.log("err");
     })
   },
+
+  constructDivs: function(u) {
+    var s = [];
+    u.forEach(function(url,i){
+      s.push(
+        <div><img src={{url}}/></div>
+      )
+    });
+    console.log(s);
+    this.setState({
+      pinImages: s
+    })
+  },
+
   render: function() {
     return(
       <Slick foo={this.state.pinImages}/>
@@ -30,18 +62,22 @@ var App = React.createClass({
 });
 
 var Slick = React.createClass({
+
   render: function() {
+    var urlArray = this.props.foo;
   	var settings = {
+      customPaging: function(i) {
+        return <div><img src='https://s-media-cache-ak0.pinimg.com/originals/6c/37/d3/6c37d3dfe8636f07ed00ae22e3b3e937.jpg'/></div>
+      },
     	dots: true
     }
     return (
-    	<div className='container'>
+    	<div className="container">
       	<Slider {...settings}>
-          <div><img src='https://s-media-cache-ak0.pinimg.com/originals/6c/37/d3/6c37d3dfe8636f07ed00ae22e3b3e937.jpg'/></div>
-          <div><img src='https://s-media-cache-ak0.pinimg.com/originals/e1/1f/81/e11f819f1b1b829db6a13d35a8e5a852.jpg'/></div>
-          <div><img src='https://s-media-cache-ak0.pinimg.com/originals/ef/58/29/ef5829590bfc4fe2a6c53da2efdaa6d2.jpg'/></div>
-          <div><img src='https://s-media-cache-ak0.pinimg.com/originals/d9/70/41/d970418835588e37e7ba52e411e2836c.jpg'/></div>
-          <div><img src='https://s-media-cache-ak0.pinimg.com/originals/4a/71/4b/4a714b04ae425f98e2022435fe433bcc.jpg'/></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
         </Slider>
       </div>
     );
@@ -51,6 +87,13 @@ var Slick = React.createClass({
 ReactDOM.render(<App />, document.getElementById("app"));
 
 /*
+(<div><img src={urlArray[i]}/></div>)
+
+<div><img src='https://s-media-cache-ak0.pinimg.com/originals/6c/37/d3/6c37d3dfe8636f07ed00ae22e3b3e937.jpg'/></div>
+<div><img src='https://s-media-cache-ak0.pinimg.com/originals/e1/1f/81/e11f819f1b1b829db6a13d35a8e5a852.jpg'/></div>
+<div><img src='https://s-media-cache-ak0.pinimg.com/originals/ef/58/29/ef5829590bfc4fe2a6c53da2efdaa6d2.jpg'/></div>
+<div><img src='https://s-media-cache-ak0.pinimg.com/originals/d9/70/41/d970418835588e37e7ba52e411e2836c.jpg'/></div>
+<div><img src='https://s-media-cache-ak0.pinimg.com/originals/4a/71/4b/4a714b04ae425f98e2022435fe433bcc.jpg'/></div>
 
 "<div><img src='https://s-media-cache-ak0.pinimg.com/originals/6c/37/d3/6c37d3dfe8636f07ed00ae22e3b3e937.jpg'/></div>",
 "<div><img src='https://s-media-cache-ak0.pinimg.com/originals/e1/1f/81/e11f819f1b1b829db6a13d35a8e5a852.jpg'/></div>",
